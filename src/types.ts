1243bc919479ec5e7b33d27ae2cec59490e86cbb
Amnon Heiman <amnonlalaux@gmail.com>
2020-12-31 20:01:16 +0200

    update the types for the plugin configuration and for the query
    This patch modify the default type for something meaningful and change
    the field to match the alertmanager fields for configuration and for
    query.
    
    src/module.ts, src/types.ts, src/ConfigEditor.tsx, src/QueryEditor.tsx

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,17 @@
 import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
-export interface MyQuery extends DataQuery {
-  queryText?: string;
-  constant: number;
+export interface AlertQuery extends DataQuery {
+  active?: boolean;
+  silenced?: boolean;
+  inhibited?: boolean;
+  unprocessed?: boolean;
+  receiver?: string;
+  filter?: string;
 }
-
-export const defaultQuery: Partial<MyQuery> = {
-  constant: 6.5,
-};
 
 /**
  * These are options configured for each DataSource instance
  */
-export interface MyDataSourceOptions extends DataSourceJsonData {
-  path?: string;
-}
-
-/**
- * Value that is used in the backend, but never sent over HTTP to the frontend
- */
-export interface MySecureJsonData {
-  apiKey?: string;
+export interface AlertDataSourceOptions extends DataSourceJsonData {
+  url: string;
 }
